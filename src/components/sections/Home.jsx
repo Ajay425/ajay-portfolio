bc16d874a9d62a35506b1ff2287e8d16b5fd1b47
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-06-11 15:12:34 -0400

    feat(hero): implement hero section buttons with hover effects

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,6 +6,10 @@ export const Home = () => {
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro quos optio vero vel fuga! Vero, impedit molestiae nihil labore eaque, repellat sequi quia tempora provident dicta itaque in maiores mollitia.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi praesentium velit itaque rerum natus? Odio enim, veniam corporis dolore tempore eius animi aut perspiciatis ullam omnis odit repudiandae, neque vero!
         </p>
+        <div className="flex justify-center space-x-4">
+            <a href="#projects" className="bg-blue-500 text white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">View Projects</a>
+            <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">View Projects</a>
+        </div>
         </div>
     </section>
 }
