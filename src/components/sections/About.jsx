099303a660105434254aa9878ef3215d5e5c6c5a
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-06-12 11:16:06 -0400

    feat: About me page

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,41 @@
+export const About = () =>  {
+
+    const FrontendSkills = ["React.js", "TailwindCSS", "Bootstrap"]
+    const backendSkills = ["Node.js", "Python", "MongoDB", "MySQL", "GraphQL", "Spring-Boot", "Firebase"]
+    
+
+    return ( 
+    <section id="about" 
+    className="min-h-screen flex items-center justify-center py-20">
+        <div className="max-w-3xl mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">About Me</h2>
+        
+
+        <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
+            <p className="text-grey-300 mb-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore iure, nesciunt sed pariatur itaque suscipit a 
+                ipsum neque quod quae omnis cumque dolorem officia facere quam natus recusandae dolores aperiam?
+            </p>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+                    <h3 className="text-xl font-bold mb-4">Frontend</h3>
+                    <div className="flex flex-wrap gap-2">
+                        {FrontendSkills.map((tech, key)=> (
+                            <span 
+                            key={key}
+                            className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                                            hover:shadow-[0_2px+8px_rgba(59, 130, 2246, 0.2)] transition
+                            ">
+                                {tech}
+                            </span>
+                        ))}
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+    );
+};
