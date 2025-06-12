46152dc914b011c1709121d7b41e9bfbab88317c
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-06-12 11:14:38 -0400

    style: change gradient color to cyan

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
     return <section id="home" className="min-h-screen flex items-center justify-center relative">
         <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">Hi, I'm Ajay Bukkaraya</h1>
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Hi, I'm Ajay Bukkaraya</h1>
         <p className="text-grey-400 text-lg mb-8 max-w-lg mx-auto">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro quos optio vero vel fuga! Vero, impedit molestiae nihil labore eaque, repellat sequi quia tempora provident dicta itaque in maiores mollitia.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi praesentium velit itaque rerum natus? Odio enim, veniam corporis dolore tempore eius animi aut perspiciatis ullam omnis odit repudiandae, neque vero!
