ba07bfd0a853b81960d46c0537a1215c540cbc1b
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-06-17 13:14:18 -0400

    chore(content): update hero section descriptions

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,13 +6,12 @@ export const Home = () => {
     <section id="home" className="min-h-screen flex items-center justify-center relative">
         <RevealOnScroll>
         <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Hi, I'm Ajay Bukkaraya</h1>
-        <p className="text-grey-400 text-lg mb-8 max-w-lg mx-auto">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro quos optio vero vel fuga! Vero, impedit molestiae nihil labore eaque, repellat sequi quia tempora provident dicta itaque in maiores mollitia.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi praesentium velit itaque rerum natus? Odio enim, veniam corporis dolore tempore eius animi aut perspiciatis ullam omnis odit repudiandae, neque vero!
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-tight">Hi, I'm Ajay Bukkaraya</h1>
+        <p className="text-grey-400 text-lg mb-7 max-w-lg mx-auto">
+            I’m a recent Computer Science graduate from York University.
         </p>
         <div className="flex justify-center space-x-4">
-            <a href="#projects" className="bg-blue-500 text white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">View Projects</a>
+            <a href="#projects" className="bg-blue-500 text white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]">View Resume</a>
             <a href="#projects" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10">View Projects</a>
         </div>
         </div>
